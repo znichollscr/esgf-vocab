a34579ed85a12009658810e5a9ab142548067767
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2025-10-20 19:38:34 +0200

    Frequency

--- src/esgvoc/api/data_descriptors/frequency.py
+++ src/esgvoc/api/data_descriptors/frequency.py
@@ -1,8 +1,50 @@
+"""
+Model (i.e. schema/definition) of the frequency data descriptor
+"""
+
 from esgvoc.api.data_descriptors.data_descriptor import PlainTermDataDescriptor
 
 
+# TODO: Karl proposed calling this ReportingInterval,
+# which is technically more accurate.
+# I'm not sure the change is worth making though.
 class Frequency(PlainTermDataDescriptor):
-    description: str
+    """
+    Reporting/temporal sampling interval used when creating the dataset
+
+    Examples: "mon", "day", "3hr", "monC"
+
+    This is a bit of a trickier concept than it first appears.
+    For time average data, it is effectively the size of each time cell
+    (e.g. if each time point is the average of a month's worth of data,
+    then the data is assigned the term "mon").
+    For time point data, it is the time interval between each reported point
+    (e.g. if the data is reported at midday each day,
+    then the data is assigned the term "day",
+    although in practice the size of each time cell works in this case too).
+
+    This can usually be validated against the actual data in the file,
+    but it can be complicated with some calendars
+    (e.g. the Julian-Gregorian calendar which has 15 missing days in 1582),
+    reporting intervals (e.g. "mon", which changes length at each interval)
+    and when climatologies are involved
+    (as identifying these follows special rules covered by the CF conventions).
+    """
+
     long_name: str
+    """Long name of this term"""
+    # TODO: figure out if this is used or needed (for CF?) or can be removed as we have description
+
     name: str
+    """Name of this term"""
+    # TODO: figure out if this is used or needed (for CF?) or can be removed as we have id and drs_name
+
+    interval: float
+    """
+    Size of the interval
+
+    See `self.unit` for units.
+    """
+
     unit: str
+    """Unit of the interval"""
