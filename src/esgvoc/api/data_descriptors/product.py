d9bd873b69ad08d96afbf30c4a0f0aac028d4196
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2025-10-21 04:49:27 +0200

    Product

--- src/esgvoc/api/data_descriptors/product.py
+++ src/esgvoc/api/data_descriptors/product.py
@@ -1,6 +1,19 @@
+"""
+Model (i.e. schema/definition) of the product data descriptor
+"""
+
 from esgvoc.api.data_descriptors.data_descriptor import PlainTermDataDescriptor
 
 
 class Product(PlainTermDataDescriptor):
-    description: str
+    """
+    Identifier of the data category
+
+    Examples: "model-output", "observations", derived"
+
+    This is not a precisely defined data descriptor,
+    rather an approximate labelling.
+    """
+
+    # TODO: delete? Redundant given we have id and drs_name
     kind: str
