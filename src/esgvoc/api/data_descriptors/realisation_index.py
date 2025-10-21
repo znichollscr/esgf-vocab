aca4808ab3b38cc00f988a07328a83a7bc0d655c
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2025-10-21 04:54:35 +0200

    Realisation index

--- src/esgvoc/api/data_descriptors/realisation_index.py
+++ src/esgvoc/api/data_descriptors/realisation_index.py
@@ -1,5 +1,28 @@
+"""
+Model (i.e. schema/definition) of the realisation index data descriptor
+"""
+
 from esgvoc.api.data_descriptors.data_descriptor import PatternTermDataDescriptor
 
 
+# TODO: check spelling: american i.e. realization or english i.e. realisation ?
 class RealisationIndex(PatternTermDataDescriptor):
-    pass
+    """
+    Label that identifies the realisation variant used to produce a dataset
+
+    Examples: "r1", "r2", "r23"
+
+    This label can be used to distinguish between two simulations
+    that are equally likely but differ only due to stochastic variations.
+    These differences can be purely stochastic
+    (i.e. arising simply from stochastic variations when re-running the same simulation,
+    even keeping all other conditions the same)
+    or arise from differences in initial conditions
+    e.g. starting/branching from different points in a control run/parent experiment.
+    The value has no intrinsic meaning within the CVs.
+    However, in other external sources (to be confirmed which)
+    the meaning of this realisation label for a given simulation can be looked up.
+    """
+
+    # TODO: in CMIP6 the regexp would have been `\d*` i.e. any integer
+    # but apparently for CMIP7 this should now be `r\d*` i.e. "r" followed by any integer.
