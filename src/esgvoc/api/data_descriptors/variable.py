964b0112177f9f5095c51e1b5dd8e11d7a0380a3
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2025-10-22 14:50:56 +0200

    Variable

--- src/esgvoc/api/data_descriptors/variable.py
+++ src/esgvoc/api/data_descriptors/variable.py
@@ -7,13 +7,15 @@ class Variable(PlainTermDataDescriptor):
     """
     A climate-related quantity or measurement.
 
+    Examples: "tas", "pr", "psl", "rlut"
+
     These quantities represent key physical, chemical or biological properties of the Earth system
     and can be the result of direct observation of the climate system or simulations.
     Variables cover a range of aspects of the climate system,
     such as temperature, precipitation, sea level, radiation, or atmospheric composition.
-    Some examples of variables that have been used in CMIP:
+    Some more information for variables that have been used in CMIP:
 
-    - *tas*: Near-surface air temperature (often measured at 2 meters above the surface)
+    - *tas*: Near-surface air temperature (measured at 2 meters above the surface)
     - *pr*: Precipitation
     - *psl*: Sea-level pressure
     - *zg*: Geopotential height
@@ -27,6 +29,32 @@ class Variable(PlainTermDataDescriptor):
     """
 
     validation_method: str = Field(default="list")
+    # TODO: discuss, what is this?
+
     long_name: str
+    """
+    Long name of the variable
+
+    This is free text and can take any value
+    """
+
+    # TODO: discuss whether we should validate that this is known by CF
+    # if it is provided.
+    # That would require somehow scraping
+    # https://cfconventions.org/Data/cf-standard-names/current/build/cf-standard-name-table.html
+    # I have asked David if there's a better way
     standard_name: str | None
+    """
+    Standard name of the variable
+
+    The standard names are defined by the CF-conventions.
+
+    If `None`, this variable has no standard name according to the CF-conventions.
+    """
+
+    # # TODO: forbid `None` i.e. change to
+    # units: str
     units: str | None
+    """
+    Units of the variable
+    """
