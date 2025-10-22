d175ec815e6463ab3f80f2ea48703962923b60f2
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2025-10-22 14:37:33 +0200

    Realm

--- src/esgvoc/api/data_descriptors/realm.py
+++ src/esgvoc/api/data_descriptors/realm.py
@@ -1,6 +1,19 @@
+"""
+Model (i.e. schema/definition) of the realm data descriptor
+"""
+
 from esgvoc.api.data_descriptors.data_descriptor import PlainTermDataDescriptor
 
 
 class Realm(PlainTermDataDescriptor):
-    description: str
+    """
+    Realm associated with the dataset
+
+    Examples: "atmos", "land", "ocean", "atmosChem"
+
+    This is intended as a rough categorisation only
+    and is not precisely defined.
+    """
+
+    # TODO: delete? redundant with description and drs_name
     name: str
